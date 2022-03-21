7b9fdeb70728007a8c9aec5c5050c36537026fbc
Chloe <chloepeng331@icloud.com>
2022-03-20 22:05:15 -0400

    complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require "pry"
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows , through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    # lists all of the characters that actor has alongside the show that the character is in.
+    def list_roles
+        self.characters.map do |character|
+          "#{character.name} - #{character.show.name}"
+        end
+      end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require "pry"
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    # returns an Array of the full names of each actor 
+    def actors_list
+        actors.map do |actor|
+        actor.full_name
+        end
+    end
 end
